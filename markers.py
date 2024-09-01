7264cc35f0b14452bceeba1d92328e5fa318fc09
batoularafa <92258144+batoularafa@users.noreply.github.com>
2024-09-01 14:15:11 +0300

    Update Gui_Mine.py
    added robot coordinates

--- markers.py
+++ markers.py
@@ -4,18 +4,21 @@ import matplotlib.colors as colors
 import matplotlib.colors as mlc
 import string
 from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
+
 
 def draw_graph():
-    global mine, img , x_coord, y_coord, value, im, fig
+    global mine, img , x_mine, y_mine, value, im, fig, x_robot, y_robot
     clr = [(1, 1, 1), '#0a3563', '#1592ad'] # white and two shades of blue
 
     # custom legends
     elment = [Patch(facecolor='#1592ad', edgecolor ='#1592ad', label ='Surface Mine'),
-            Patch(facecolor='#0a3563', edgecolor ='#0a3563', label ='Buried Mine')]
+            Patch(facecolor='#0a3563', edgecolor ='#0a3563', label ='Buried Mine'),
+            Line2D([0], [0], marker='o', label='Robot', markersize=10,  linewidth=0)]
 
     # axis labels
-    y_axis = []
     x_axis =  string.ascii_uppercase[:19] 
+    y_axis = []
     for i in range(19,0,-1):
         y_axis.append(str(i))
 
@@ -23,9 +26,11 @@ def draw_graph():
     img =  np.zeros((19,19), dtype=float) #values of the cmap/ the colors (0.0,1.0,2.0)
 
     #initialize coordinates and mine state
-    x_coord= 0 #(1-19)
-    y_coord= '' #(A-S)
+    x_mine= '' #(A-S)
+    y_mine= 0  #(1-19)
     value = 0  # 0- no mines , 1- buried mine, 2- surface mine
+    x_robot = ''
+    y_robot = 0
 
     cmap = mlc.LinearSegmentedColormap.from_list('ColorMap', clr)
     norm = colors.BoundaryNorm([0, 1, 2, 3], cmap.N)
@@ -33,6 +38,7 @@ def draw_graph():
     im = ax.imshow(img, cmap=cmap, norm =  norm)
 
     # Show all ticks and label them with the respective list entries
+    # ax.spines[:].set_visible(False)
     ax.set_xticks(np.arange(len(x_axis)), labels=x_axis)
     ax.set_yticks(np.arange(len(y_axis)), labels=y_axis)
     ax.set_xticks(np.arange(len(x_axis))-.5, minor=True)
@@ -43,17 +49,22 @@ def draw_graph():
     plt.legend(handles = elment, loc = 'upper left',bbox_to_anchor=(1,1))
     ax.set_title("Mines Map Representation")
     fig.tight_layout()
-
-    plt.ion() #interative mode, the window will not block the execution of the script
+    
+    plt.ion() #interactive mode, the window will not block the execution of the script
     plt.show(block=False) # show the graph without blocking the script
-
+    
 def update_mine():
-    x_coord = input("Enter x coordinate (1-19): ")
-    y_coord = input("Enter y coordinate (A-S): ")
+    x_mine = input("Enter x coordinate (A-S): ")
+    y_mine = input("Enter y coordinate (1-19): ")
     value = input("Enter mine state (0-2): ")
-    x_coord = 19 - int(x_coord)
-    y_coord = ord(y_coord) - 65
-    mine[x_coord][y_coord] = int(value)
+    x_mine = ord(x_mine) - 65
+    y_mine = 19 - int(y_mine)
+    mine[y_mine][x_mine] = int(value) #x and y are inverted
+
+    x_robot, y_robot = input("enter robot coordinates(char, int):").split(',')
+    x_robot = ord(x_robot) - 65
+    y_robot = 19 - int(y_robot)
+    
     for i in range(19):
         for j in range(19):
             if mine[i][j] == 0:
@@ -62,6 +73,7 @@ def update_mine():
                 img[i][j]=1.0 # (second color) >> dark blue
             else:
                 img[i][j]=2.0  # (third color)
+    plt.plot(x_robot,y_robot, linestyle='None', marker = 'o')
     im.set_data(img) # update the data of the img array 
     fig.canvas.draw_idle #redraw the graph
 
