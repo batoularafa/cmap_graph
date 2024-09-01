dd16007e408cb86c6f26e26d2d36784f4e66ec1c
batoularafa <92258144+batoularafa@users.noreply.github.com>
2024-09-01 22:21:12 +0300

    Update Graph_Mines.py

--- Graph_Mines.py
+++ Graph_Mines.py
@@ -6,9 +6,9 @@ import string
 from matplotlib.patches import Patch
 from matplotlib.lines import Line2D
 
-
+marker = None
 def draw_graph():
-    global mine, img , x_mine, y_mine, value, im, fig, x_robot, y_robot
+    global mine, img , x_mine, y_mine, value, im, fig,ax
     clr = [(1, 1, 1), '#0a3563', '#1592ad'] # white and two shades of blue
 
     # custom legends
@@ -18,9 +18,7 @@ def draw_graph():
 
     # axis labels
     x_axis =  string.ascii_uppercase[:19] 
-    y_axis = []
-    for i in range(19,0,-1):
-        y_axis.append(str(i))
+    y_axis = [str(i) for i in range(19, 0, -1)]
 
     mine =  np.zeros((19,19)) # buried, surface or no mine
     img =  np.zeros((19,19), dtype=float) #values of the cmap/ the colors (0.0,1.0,2.0)
@@ -29,8 +27,7 @@ def draw_graph():
     x_mine= '' #(A-S)
     y_mine= 0  #(1-19)
     value = 0  # 0- no mines , 1- buried mine, 2- surface mine
-    x_robot = ''
-    y_robot = 0
+
 
     cmap = mlc.LinearSegmentedColormap.from_list('ColorMap', clr)
     norm = colors.BoundaryNorm([0, 1, 2, 3], cmap.N)
@@ -54,16 +51,20 @@ def draw_graph():
     plt.show(block=False) # show the graph without blocking the script
     
 def update_mine():
+    global marker
     x_mine = input("Enter x coordinate (A-S): ")
     y_mine = input("Enter y coordinate (1-19): ")
     value = input("Enter mine state (0-2): ")
     x_mine = ord(x_mine) - 65
     y_mine = 19 - int(y_mine)
     mine[y_mine][x_mine] = int(value) #x and y are inverted
-
-    # x_robot, y_robot = input("enter robot coordinates(char, int):").split(',')
-    # x_robot = ord(x_robot) - 65
-    # y_robot = 19 - int(y_robot)
+    x_robot, y_robot = input("enter robot coordinates(char, int):").split(',')
+    
+    if marker:  # Check if marker exists
+        marker.remove()
+    x_robot = ord(x_robot) - 65
+    y_robot = 19 - int(y_robot)
+    marker = plt.plot(x_robot,y_robot, linestyle='None', marker = 'o', color='#1592ad')[0]
     
     for i in range(19):
         for j in range(19):
@@ -73,7 +74,7 @@ def update_mine():
                 img[i][j]=1.0 # (second color) >> dark blue
             else:
                 img[i][j]=2.0  # (third color)
-    # plt.plot(x_robot,y_robot, linestyle='None', marker = 'o')
+    
     im.set_data(img) # update the data of the img array 
     fig.canvas.draw_idle #redraw the graph
 
